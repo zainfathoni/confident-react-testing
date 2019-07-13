73cc7d83b28b9a8bc10f3e2b7f8b88511b5197c3
Zain Fathoni <zain.fathoni@gmail.com>
2019-07-14 02:24:45 +0800

    Add Demo 3 slides

--- deck.js
+++ deck.js
@@ -2,7 +2,14 @@ export { nightOwlFull as theme } from 'code-surfer'
 
 import { slides as demo1 } from './demo-1/demo-1.mdx'
 import { slides as demo2 } from './demo-2/demo-2.mdx'
+import { slides as demo3 } from './demo-3/demo-3.mdx'
 import { slides as example } from './example.mdx'
 import { slides as introduction } from './introduction.mdx'
 
-export const slides = [...introduction, ...demo1, ...demo2, ...example]
+export const slides = [
+  ...introduction,
+  ...demo1,
+  ...demo2,
+  ...demo3,
+  ...example,
+]
---
+++ demo-3/Toggle.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Button from './ButtonMistake'
+
+class Toggle extends React.Component {
+  state = {
+    toggle: false,
+  }
+
+  handleToggle = () => {
+    this.setState({ toggle: !this.state.toggle })
+  }
+
+  render() {
+    return (
+      <div>
+        <span>{this.state.toggle ? 'On' : 'Off'}</span>
+        <Button onClick={this.handleToggle}>Toggle</Button>
+      </div>
+    )
+  }
+}
+
+export default Toggle
