e7bf80a3dd20106c5f86dd6c876914002ec0db6f
Zain Fathoni <zain.fathoni@gmail.com>
2019-07-14 02:08:47 +0800

    Add Swap Toggle with ToggleHooks section

---
+++ demo-2/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Toggle from './Toggle'
+
+function App() {
+  return <Toggle />
+}
+
+export default App
---
+++ demo-2/AppHooks.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Toggle from './ToggleHooks'
+
+function App() {
+  return <Toggle />
+}
+
+export default App
