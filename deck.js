b143d58b7ac3bb42c0d4284354f91f8be1b086d1
Zain Fathoni <zain.fathoni@gmail.com>
2019-07-13 23:43:00 +0800

    Add Toggle Class Component slide

--- deck.js
+++ deck.js
@@ -1,6 +1,11 @@
 export { nightOwlFull as theme } from "code-surfer";
 
+import { slides as demo1 } from "./demo-1.mdx";
 import { slides as example } from "./example.mdx";
 import { slides as introduction } from "./introduction.mdx";
 
-export const slides = [...introduction, ...example];
+export const slides = [
+  ...introduction,
+  ...demo1,
+  ...example,
+];
