31489c28dc358c784f018d5137abb9a715142cc1
Zain Fathoni <zain.fathoni@gmail.com>
2019-07-13 08:53:47 +0800

    Add example deck

---
+++ deck.js
@@ -0,0 +1,6 @@
+export { nightOwlFull as theme } from "code-surfer";
+
+import { slides as example } from "./example.mdx";
+import { slides as introduction } from "./introduction.mdx";
+
+export const slides = [...introduction, ...example];
