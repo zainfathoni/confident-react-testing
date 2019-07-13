3010599ac6dd401cf86a0f0265453fc57c9a07e0
Zain Fathoni <zain.fathoni@gmail.com>
2019-07-14 03:31:29 +0800

    Add cleanup step in the Solution section

---
+++ solution/Toggle.test.5.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
+import Toggle from './Toggle'
+
+describe('Test Toggle using React Testing Library', () => {
+  it('render Button component', () => {
+    const { getByText, queryByText } = render(<Toggle />)
+
+    expect(getByText('Off')).toBeDefined()
+    expect(queryByText('On')).toBeNull()
+
+    fireEvent.click(getByText('Toggle'))
+
+    expect(getByText('On')).toBeDefined()
+    expect(queryByText('Off')).toBeNull()
+  })
+})
