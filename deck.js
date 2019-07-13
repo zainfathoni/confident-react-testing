274f87a406cbf8de0f5c8c61240a8184fcc45637
Zain Fathoni <zain.fathoni@gmail.com>
2019-07-14 04:00:15 +0800

    Add Problem Statements section

--- deck.js
+++ deck.js
@@ -1,14 +1,16 @@
 export { nightOwlFull as theme } from 'code-surfer'
 
+import { slides as comparison } from './comparison/comparison.mdx'
 import { slides as demo1 } from './demo-1/demo-1.mdx'
 import { slides as demo2 } from './demo-2/demo-2.mdx'
 import { slides as demo3 } from './demo-3/demo-3.mdx'
 import { slides as introduction } from './introduction.mdx'
-import { slides as comparison } from './comparison/comparison.mdx'
 import { slides as solution } from './solution/solution.mdx'
+import { slides as problemStatements } from './problem-statements.mdx'
 
 export const slides = [
   ...introduction,
+  ...problemStatements,
   ...demo1,
   ...demo2,
   ...demo3,
