047ea04f5252146f616db26d53cb9f2ad4bdc589
Zain Fathoni <zain.fathoni@gmail.com>
2019-07-16 15:59:52 +0700

    Refactor App.js & AppHooks.js

--- solution/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Toggle from './Toggle'
-
-function App() {
-  return <Toggle />
-}
-
-export default App
--- solution/AppHooks.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Toggle from './ToggleHooks'
-
-function App() {
-  return <Toggle />
-}
-
-export default App
